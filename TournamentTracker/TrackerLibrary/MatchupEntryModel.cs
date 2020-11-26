23cce0375f58ab9401e18b4d974e011f124791cc
dabina2018 <daveena.cooper@outlook.com>
2020-11-26 16:00:59 -0600

    adding project files

---
+++ TournamentTracker/TrackerLibrary/MatchupEntryModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackerLibrary
+{
+    public class MatchupEntryModel
+    {
+        /// <summary>
+        /// write a description here... Represents one team in teh matchup
+        /// </summary>
+        public TeamModel TeamCompeting { get; set; }
+        /// <summary>
+        /// Represents the score for this particular team
+        /// </summary>
+        public double Score { get; set; }
+        /// <summary>
+        /// Represents the score for the matchup that this team came from as the winner.
+        /// </summary>
+        public MatchupModel ParentMatchup { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public MatchupEntryModel(double initialScore)
+        {
+            Console.WriteLine();
+        }
+    }
+}
