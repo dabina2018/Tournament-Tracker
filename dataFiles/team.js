0f1a19b6082be6b58c8e1778ac1d430bf4410d3d
dabina2018 <35677021+dabina2018@users.noreply.github.com>
2022-08-04 20:26:14 -0400

    ugh

--- dataFiles/team.js
+++ dataFiles/team.js
@@ -1,7 +1,16 @@
 //https://www.digitalocean.com/community/tutorials/how-to-read-and-write-csv-files-in-node-js-using-node-csv
 
+const express = require('express');
 const fs = require('fs');
 const { parse } = require('csv-parse');
+
+const app = express()
+
+//app.METHOD(PATH, HANDLER)
+app.use(express.json());
+app.get('/', function (req :Request<P, ResBody, ReqBody, ReqQuery, Locals>, res: Response<ResBody,Locals> ) {
+    res.json('This is a test')
+})
 
 var parser = parse({columns: true}, function (err, records) {
     console.log(records);
