1a196984f1d85c04e4a64059e885766ab7ecf2e3
dabina2018 <35677021+dabina2018@users.noreply.github.com>
2022-08-07 12:32:03 -0400

    trying new approach for dynamic table

---
+++ pages/createPrize.js
@@ -0,0 +1,11 @@
+export default class{
+    /**
+     * 
+     * @param {HTMLTableElement} root The table element which will display CSV data.
+     */
+    constructor(root){
+        this.root = root;
+        console.log("I am constructed!")
+
+    }
+}
---
+++ pages/main.js
@@ -0,0 +1,4 @@
+import createPrize from "./createPrize.js";
+
+const tableRoot = document.querySelector("csvRoot");  //table Id from index file
+const createPrize = new createPrize(tableRoot);
