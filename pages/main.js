af4fbd6dc537c1ba59e4f095fd26fcca5cfeeb4e
dabina2018 <35677021+dabina2018@users.noreply.github.com>
2022-08-07 12:36:06 -0400

    fixed class declaration

--- pages/main.js
+++ pages/main.js
@@ -1,4 +1,4 @@
-import createPrize from "./createPrize.js";
+import CreatePrize from "./CreatePrize.js";
 
 const tableRoot = document.querySelector("csvRoot");  //table Id from index file
-const createPrize = new createPrize(tableRoot);
+const createPrize = new CreatePrize(tableRoot);
