b9a9aa34ac5290a690a8d5a3eaa322311f263400
dabina2018 <35677021+dabina2018@users.noreply.github.com>
2022-08-07 16:18:27 -0400

    adding css

--- pages/main.js
+++ pages/main.js
@@ -17,10 +17,4 @@ csvFileInput.addEventListener("change", e => {
     })
 });
 
-/*
-createPrize.update([
-    [1, "positive", 99, "$100"],
-    [2, "broke even", 99, 8908],
-    [3, "negative", 99, "$400"]
-], ["ID", "Revenue", "Profit", "Cost"])
-*/
+
