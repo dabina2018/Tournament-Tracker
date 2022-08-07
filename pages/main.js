9900091f983ad8cb8f53937468c7240945475cdf
dabina2018 <35677021+dabina2018@users.noreply.github.com>
2022-08-07 14:50:36 -0400

    adding csv file in the mix

--- pages/main.js
+++ pages/main.js
@@ -1,11 +1,26 @@
 import CreatePrize from "./CreatePrize.js";
 
 const tableRoot = document.querySelector("#csvRoot");  //table Id from index file
+const csvFileInput = document.querySelector("#csvFileInput");
 const createPrize = new CreatePrize(tableRoot);
 
+csvFileInput.addEventListener("change", e => {
+    console.log(csvFileInput.files[0]);
+
+    Papa.parse(csvFileInput.files[0], {
+        delimiter: ",",
+        skipEmptyLines: true,
+        complete: results =>{
+            console.log(results);
+        }
+    })
+
+});
+
+/*
 createPrize.update([
     [1, "positive", 99, "$100"],
     [2, "broke even", 99, 8908],
     [3, "negative", 99, "$400"]
 ], ["ID", "Revenue", "Profit", "Cost"])
-
+*/
