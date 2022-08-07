b3bf1aa410e206f518d4502a2c78154981c8b188
dabina2018 <35677021+dabina2018@users.noreply.github.com>
2022-08-07 13:51:05 -0400

    tweaking column ids and values

--- pages/main.js
+++ pages/main.js
@@ -3,9 +3,9 @@ import CreatePrize from "./CreatePrize.js";
 const tableRoot = document.querySelector("#csvRoot");  //table Id from index file
 const createPrize = new CreatePrize(tableRoot);
 
-createPrize.setHeader(["ID", "Name", "Age"]);
+createPrize.setHeader(["ID", "Revenue", "Profit", "Cost"]);
 createPrize.setBody([
-    [4500, "daily", 99],
-    [4500, "daily", 99],
-    [4500, "daily", 99]
+    [1, "positive", 99, "$100"],
+    [2, "broke even", 99, 8908],
+    [3, "negative", 99, "$400"]
 ]);
