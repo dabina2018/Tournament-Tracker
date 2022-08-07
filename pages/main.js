4478233f3e048b3b2eca108d069fb3753ab04901
dabina2018 <35677021+dabina2018@users.noreply.github.com>
2022-08-07 18:46:56 -0400

    hardcoding excel file

--- pages/main.js
+++ pages/main.js
@@ -2,8 +2,8 @@
 import CreatePrize from "./CreatePrize.js";
 
 const tableRoot = document.querySelector("#csvRoot");  //table Id from index file
-const csvFileInput = document.querySelector("#csvFileInput");
-//const csvFileInput = document(/Tournament-Tracker/dataFiles/TournamentModels.csv)
+//const csvFileInput = document.querySelector("#csvFileInput");
+const csvFileInput = document(/Tournament-Tracker/dataFiles/TournamentModels.csv)
 const createPrize = new CreatePrize(tableRoot);
 
 
