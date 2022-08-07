c45f1c107856b5654f40c518457bb247c643df45
dabina2018 <35677021+dabina2018@users.noreply.github.com>
2022-08-07 18:59:10 -0400

    hardcoding excel file

--- pages/main.js
+++ pages/main.js
@@ -1,8 +1,8 @@
 //import Papa from 'papaparse';
 import CreatePrize from "./CreatePrize.js";
 
-const tableRoot = document.querySelector("#csvRoot");  //table Id from index file
-//const csvFileInput = document.querySelector("#csvFileInput");
+//const tableRoot = document.querySelector("#csvRoot");  //table Id from index file
+const csvFileInput = document.querySelector("#csvFileInput");
 const csvFileInput = document(/Tournament-Tracker/dataFiles/TournamentModels.csv)
 const createPrize = new CreatePrize(tableRoot);
 
