5e65ffc916c1338e3359d660eacc96c52199ac11
dabina2018 <35677021+dabina2018@users.noreply.github.com>
2022-08-08 16:51:33 -0400

    adding new method

--- pages/main.js
+++ pages/main.js
@@ -3,10 +3,6 @@ import CreatePrize from "./CreatePrize.js";
 
 const tableRoot = document.querySelector("#csvRoot");  //table Id from html file
 const csvFileInput = document.querySelector("#csvFileInput");
-
-//Open csv file
-//$file =fgetcsv('/Tournament-Tracker/dataFiles/TournamentModels.csv', "r")
-//const csvFileInput = '/Tournament-Tracker/dataFiles/TournamentModels.csv'
 const createPrize = new CreatePrize(tableRoot);
 
 csvFileInput.addEventListener("change", e => {
@@ -22,5 +18,20 @@ csvFileInput.addEventListener("change", e => {
     })
 });
 
+//Open csv file
+//$file =fgetcsv('/Tournament-Tracker/dataFiles/TournamentModels.csv', "r")
+var csvContent = '/Tournament-Tracker/dataFiles/TournamentModels.csv';
+$("viewAll").click(function() {
+    console.log(csvFileInput.files[0]);
+
+    Papa.parse(csvFileInput.files[0], {
+        delimiter: ",",
+        skipEmptyLines: true,
+        complete: results =>{
+            createPrize.update(results.data.slice(0), results.data[0]);
+        }
+    })
+
+});
 
 
