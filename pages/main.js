b2905fa058a0e0f9fb87981f938f0c4aa3e8b952
dabina2018 <35677021+dabina2018@users.noreply.github.com>
2022-08-07 20:24:51 -0400

    adding file open

--- pages/main.js
+++ pages/main.js
@@ -3,8 +3,11 @@ import CreatePrize from "./CreatePrize.js";
 
 const tableRoot = document.querySelector("#csvRoot");  //table Id from index file
 //const csvFileInput = document.querySelector("#csvFileInput");
+//Open csv file
+$file =fopen('/Tournament-Tracker/dataFiles/TournamentModels.csv', "r")
 const csvFileInput = '/Tournament-Tracker/dataFiles/TournamentModels.csv'
 const createPrize = new CreatePrize(tableRoot);
+
 
 
 //csvFileInput.addEventListener("change", e => {
