90abc205d195590b72a36e47ea4eda4069a8c20d
dabina2018 <35677021+dabina2018@users.noreply.github.com>
2022-08-07 21:13:46 -0400

    new appraoch

--- pages/main.js
+++ pages/main.js
@@ -1,19 +1,16 @@
 //import Papa from 'papaparse';
 import CreatePrize from "./CreatePrize.js";
 
-const tableRoot = document.querySelector("#csvRoot");  //table Id from index file
-//const csvFileInput = document.querySelector("#csvFileInput");
+const tableRoot = document.querySelector("#csvRoot");  //table Id from html file
+const csvFileInput = document.querySelector("#csvFileInput");
+
 //Open csv file
 //$file =fgetcsv('/Tournament-Tracker/dataFiles/TournamentModels.csv', "r")
 //const csvFileInput = '/Tournament-Tracker/dataFiles/TournamentModels.csv'
 const createPrize = new CreatePrize(tableRoot);
 
-
-
 csvFileInput.addEventListener("change", e => {
-window.onload = () => {
 
-}
     console.log(csvFileInput.files[0]);
 
     Papa.parse(csvFileInput.files[0], {
